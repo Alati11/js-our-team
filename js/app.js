608d01d892596ad1d206e7334f2f316a54c27216
Alati11 <peppealati@gmail.com>
2023-10-18 15:49:30 +0200

    print console our team

--- js/app.js
+++ js/app.js
@@ -1,41 +1,48 @@
-console.log("");
+console.log(" our team");
 
 // - Creare l’array di oggetti con le informazioni fornite.
 const team = [
 	{
 		nome: 'Angela',
 		cognome: 'Caroll',
-        ruolo : 'chief-editor',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg',
 	},
 	{
 		nome: 'Angela',
 		cognome: 'Lopez',
-        ruolo: 'social-media-manager',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg',
 	},
 	{
 		nome: 'Barbara',
 		cognome: 'Ramos',
-        ruolo : 'graphic-designer',
+        ruolo : 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg',
 	},
 	{
 		nome: 'Scott',
 		cognome: 'Estrada',
-        ruolo: 'developer',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg',
 	},
     {
 		nome: 'Walter',
 		cognome: 'Gordon',
-        ruolo: 'office-manager',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg',
 	},
     {
 		nome: 'Wayne',
 		cognome: 'Barnett',
-        ruolo: 'founder-ceo',
+        ruolo: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg',
 	},
 ]
-console.log(team);
+// console.log(team);
 
 // - Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+console.log(team[0],team[1], team[2], team[3], team[4], team[5]);
 
 // - Stampare le informazioni di ogni membro su DOM sottoforma di stringhe
 
