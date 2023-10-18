8c42f04d9613746cade374e2ac804222b285baea
Alati11 <peppealati@gmail.com>
2023-10-18 15:28:17 +0200

    create array team

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 console.log("");
 
 // - Creare l’array di oggetti con le informazioni fornite.
+const team = [
+	{
+		nome: 'Angela',
+		cognome: 'Caroll',
+        ruolo : 'chief-editor',
+	},
+	{
+		nome: 'Angela',
+		cognome: 'Lopez',
+        ruolo: 'social-media-manager',
+	},
+	{
+		nome: 'Barbara',
+		cognome: 'Ramos',
+        ruolo : 'graphic-designer',
+	},
+	{
+		nome: 'Scott',
+		cognome: 'Estrada',
+        ruolo: 'developer',
+	},
+    {
+		nome: 'Walter',
+		cognome: 'Gordon',
+        ruolo: 'office-manager',
+	},
+    {
+		nome: 'Wayne',
+		cognome: 'Barnett',
+        ruolo: 'founder-ceo',
+	},
+]
+console.log(team);
 
 // - Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 
