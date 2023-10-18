6a5efd3d3160dc38bd265be40197ccf649acd13c
Alati11 <peppealati@gmail.com>
2023-10-18 17:44:46 +0200

    error print DOM

--- js/app.js
+++ js/app.js
@@ -1,52 +1,55 @@
 console.log(" our team");
+let ourTeam = " "
 
 // - Creare l’array di oggetti con le informazioni fornite.
 const team = [
 	{
 		nome: 'Angela',
 		cognome: 'Caroll',
-        ruolo: 'Chief Editor',
-        foto: 'angela-caroll-chief-editor.jpg',
+		ruolo: 'Chief Editor',
+		foto: 'angela-caroll-chief-editor.jpg',
 	},
 	{
 		nome: 'Angela',
 		cognome: 'Lopez',
-        ruolo: 'Social Media Manager',
-        foto: 'angela-lopez-social-media-manager.jpg',
+		ruolo: 'Social Media Manager',
+		foto: 'angela-lopez-social-media-manager.jpg',
 	},
 	{
 		nome: 'Barbara',
 		cognome: 'Ramos',
-        ruolo : 'Graphic Designer',
-        foto: 'barbara-ramos-graphic-designer.jpg',
+		ruolo: 'Graphic Designer',
+		foto: 'barbara-ramos-graphic-designer.jpg',
 	},
 	{
 		nome: 'Scott',
 		cognome: 'Estrada',
-        ruolo: 'Developer',
-        foto: 'scott-estrada-developer.jpg',
+		ruolo: 'Developer',
+		foto: 'scott-estrada-developer.jpg',
 	},
-    {
+	{
 		nome: 'Walter',
 		cognome: 'Gordon',
-        ruolo: 'Office Manager',
-        foto: 'walter-gordon-office-manager.jpg',
+		ruolo: 'Office Manager',
+		foto: 'walter-gordon-office-manager.jpg',
 	},
-    {
+	{
 		nome: 'Wayne',
 		cognome: 'Barnett',
-        ruolo: 'Founder & CEO',
-        foto: 'wayne-barnett-founder-ceo.jpg',
+		ruolo: 'Founder & CEO',
+		foto: 'wayne-barnett-founder-ceo.jpg',
 	},
 ]
 // console.log(team);
 
 // - Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // console.log(team[0],team[1], team[2], team[3], team[4], team[5]);
-for (let i=0; i < team.length; i++){
-    let ourTeam = team[i];
-    console.log(ourTeam);
+for (let i = 0; i < team.length; i++) {
+	let ourTeam = team[i];
+	console.log(ourTeam);
 }
 
 // - Stampare le informazioni di ogni membro su DOM sottoforma di stringhe
-
+const cardElement = document.querySelectorAll('.card');
+console.log(cardElement);
+cardElement.innerHTML = `<div class="card">${ourTeam}</div>`;
