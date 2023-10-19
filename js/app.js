fa308252d4a93089add6221234e28dda895d41f4
Alati11 <peppealati@gmail.com>
2023-10-19 15:05:21 +0200

    print card innerHTML

--- js/app.js
+++ js/app.js
@@ -1,55 +1,65 @@
 console.log(" our team");
-let ourTeam = " "
 
 // - Creare l’array di oggetti con le informazioni fornite.
 const team = [
 	{
-		nome: 'Angela',
-		cognome: 'Caroll',
+		nome: 'Angela Caroll',
 		ruolo: 'Chief Editor',
-		foto: 'angela-caroll-chief-editor.jpg',
+		foto: './img/angela-caroll-chief-editor.jpg',
 	},
 	{
-		nome: 'Angela',
-		cognome: 'Lopez',
+		nome: 'Angela Lopez',
 		ruolo: 'Social Media Manager',
-		foto: 'angela-lopez-social-media-manager.jpg',
+		foto: './img/angela-lopez-social-media-manager.jpg',
 	},
 	{
-		nome: 'Barbara',
-		cognome: 'Ramos',
+		nome: 'Barbara Ramos',
 		ruolo: 'Graphic Designer',
-		foto: 'barbara-ramos-graphic-designer.jpg',
+		foto: './img/barbara-ramos-graphic-designer.jpg',
 	},
 	{
-		nome: 'Scott',
-		cognome: 'Estrada',
+		nome: 'Scott Estrada',
 		ruolo: 'Developer',
-		foto: 'scott-estrada-developer.jpg',
+		foto: './img/scott-estrada-developer.jpg',
 	},
 	{
-		nome: 'Walter',
-		cognome: 'Gordon',
+		nome: 'Walter Gordon',
 		ruolo: 'Office Manager',
-		foto: 'walter-gordon-office-manager.jpg',
+		foto: './img/walter-gordon-office-manager.jpg',
 	},
 	{
-		nome: 'Wayne',
-		cognome: 'Barnett',
+		nome: 'Wayne Barnett',
 		ruolo: 'Founder & CEO',
-		foto: 'wayne-barnett-founder-ceo.jpg',
+		foto: './img/wayne-barnett-founder-ceo.jpg',
 	},
 ]
-// console.log(team);
+console.log(team);
 
 // - Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // console.log(team[0],team[1], team[2], team[3], team[4], team[5]);
+const cardElement = document.querySelector('.card');
+
+
+
 for (let i = 0; i < team.length; i++) {
 	let ourTeam = team[i];
 	console.log(ourTeam);
+
+	const nome = ourTeam.nome
+	const ruolo = ourTeam.ruolo
+	const foto = ourTeam.foto
+
+	// - Stampare le informazioni di ogni membro su DOM sottoforma di stringhe
+	const html = `
+	<div class="card">
+	  <img src="${foto}">
+	  <h3>${nome}</h3>
+	  <p>${ruolo}</p>
+	</div>
+	`
+	cardElement.innerHTML += html
+	console.log(cardElement);
 }
 
-// - Stampare le informazioni di ogni membro su DOM sottoforma di stringhe
-const cardElement = document.querySelectorAll('.card');
-console.log(cardElement);
-cardElement.innerHTML = `<div class="card">${ourTeam}</div>`;
+
+
